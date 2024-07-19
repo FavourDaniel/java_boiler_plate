f052aac26e5ffaa846e93d00958c5bd9fc50217d
Favour Daniel <favourd205@gmail.com>
2024-07-20 00:08:27 +0100

    deployment-fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd ~/java_boiler_plate/
 # git pull 
+cd ~
 ./stop-app.sh
+cd ~/java_boiler_plate/
 mvn dependency:resolve
 ./mvnw clean install
 sudo -S nohup ./mvnw spring-boot:run > app.log 2>&1 &
